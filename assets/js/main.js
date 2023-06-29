8d11e16ee074e0c1c3aa6539a20fd4070071d91c
Trieu Thanh <tvthanh.cod@gmail.com>
2023-06-29 10:54:41 +0700

    code done!

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,12 +14,6 @@ $(function () {
         $('.header__content-nav li').each(function (index) {
             handleActiveNav(location, index, 5)
         })
-        $('.personal-information__left-side-taglist li').each(function (index) {
-            if (location >= $(`.page-${index + 1}`).offset().top - 100 && $(`.part-${index + 2}`) != undefined ? location < $(`.part-${index + 2}`).offset().top : true) {
-                $('.personal-information__left-side-taglist li p').removeClass('active-tag')
-                $(`.personal-information__left-side-taglist li p:nth-child(${index + 1})`).addClass('active-tag')
-            }
-        })
     }) // đóng của hàm sự kiện scroll window
 
     // sự kiện auto active nav khi scroll
@@ -31,6 +25,15 @@ $(function () {
             handleScroll(part)
         })
     })// kết thúc
+
+    $('.personal-information__left-side-taglist li').each(function (index) {
+        $(`.personal-information__left-side-taglist li:nth-child(${index + 1})`).on('click', function () {
+            $('.personal-information__left-side-taglist li p').removeClass('active-tag')
+            $(`.personal-information__left-side-taglist li:nth-child(${index + 1}) p`).addClass('active-tag')
+            let part = $(`.page-${index + 1}`)
+            handleScroll(part)
+        })
+    })
 
     // hàm scroll
     function handleScroll(el) {
@@ -45,8 +48,11 @@ $(function () {
         }
     }
 
-    $('.tag__content').parent().click(function () {
-        $('.personal-information__left-side-taglist li p').addClass('active-tag')
+    $('.bars').on('click', function () {
+        $('.header__content-nav').addClass('open')
+    })
+    $('.close').on('click', function () {
+        $('.header__content-nav').removeClass('open')
     })
 })
 
